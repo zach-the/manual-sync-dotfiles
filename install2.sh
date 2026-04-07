12300100dd8cf55822bffe2c8b7c5a6c1c9b8b27
Your Name <you@example.com>
2026-04-06 22:24:25 -0600

    $1

---
+++ install2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "linking nvim init.lua"
+mkdir -p ~/.config/nvim
+[ -f ~/.config/nvim/init.lua ] && mv ~/.config/nvim/init.lua{,.old}
+ln -s ~/manual-sync-dotfiles/nvim-init.lua ~/.config/nvim/init.lua
+
+echo "linking bash stuff"
+[ -f ~/.bashrc ] && mv ~/.bashrc{,.old}
+[ -f ~/.bash_aliases ] && mv ~/.bash_aliases{,.old}
+ln -s ~/manual-sync-dotfiles/bashrc ~/.bashrc
+ln -s ~/manual-sync-dotfiles/bash_aliases ~/.bash_aliases
+
+echo "linking kitty conf"
+mkdir -p ~/.config/kitty
+[ -f ~/.config/kitty/kitty.conf ] && mv ~/.config/kitty/kitty.conf{,.old}
+ln -s ~/manual-sync-dotfiles/kitty-conf ~/.config/kitty/kitty.conf
+git clone https://github.com/kovidgoyal/kitty-themes.git ~/kitty-themes > /dev/null 2>&1
+rm -rf ~/.config/kitty/themes
+mv ~/kitty-themes/themes ~/.config/kitty/themes
+[ -f ~/.config/kitty/theme.conf ] && mv ~/.config/kitty/theme.conf{,.old}
+ln -s ~/.config/kitty/themes/Molokai.conf ~/.config/kitty/theme.conf
+rm -rf ~/kitty-themes
+
+echo "linking waybar"
+[ -d ~/.config/waybar ] && mv ~/.config/waybar{,.old}
+ln -s ~/manual-sync-dotfiles/waybar ~/.config/waybar
+
+echo "linking mpv conf"
+mkdir -p ~/.config/mpv
+[ -f ~/.config/mpv/mpv.conf ] && mv ~/.config/mpv/mpv.conf{,.old}
+
---
+++ removejunk.sh
@@ -0,0 +1,7 @@
+rm .config/nvim/init.lua.old
+rm .bashrc.old
+rm .bash_aliases.old
+rm .config/kitty/kitty.conf.old
+rm .config/kitty/theme.conf.old
+rm .config/mpv/mpv.old
+rm -rf .config/waybar.old
